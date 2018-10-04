eb2fa99682d5101862f3d94042e877b287bd7ad7
Kelly Russell <russellk10@nku.edu>
2018-10-04 01:00:34 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="css/style.css">    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+<script type="text/javascript" src="js/main.js"></script>
+<head>
+    <title>Second Chance's Animal Quiz!</title>
+</head>
+
+<body>
+
+    <header>Choose the animal based on dice roll!</header>
+
+    <!-- Animal icon menu for 12 animal question choices -->
+    <div id="chooseAnimal">
+        <div class="animalIcon" id="raccoon">&nbsp;raccoon</div>
+        <div class="animalIcon" id="opossum">&nbsp;opossum</div>
+        <div class="animalIcon" id="barnOwl">&nbsp;barn owl</div>
+        <div class="animalIcon" id="bat">&nbsp;bat</div>
+        <div class="animalIcon" id="skunk">&nbsp;skunk</div>
+        <div class="animalIcon" id="beaver">&nbsp;beaver</div>
+        <div class="animalIcon" id="turtle">&nbsp;turtle</div>
+        <div class="animalIcon" id="fox">&nbsp;fox</div>
+        <div class="animalIcon" id="groundhog">&nbsp;groundhog</div>
+        <div class="animalIcon" id="deer">&nbsp;deer</div>
+        <div class="animalIcon" id="squirrel">&nbsp;squirrel</div>
+        <div class="animalIcon" id="wildCard">&nbsp;wild card</div>
+
+    </div>
+
+    <footer></footer>
+</body>
+
+
+</html>
---
+++ qanda.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="css/style.css">
+<script type="text/javascript" src="js/main.js"></script>
+<head>
+    <title>Second Chance's Animal Quiz!</title>
+</head>
+
+<body>
+    <nav id ="goBackButton">
+        <span class="iconColor"><i class="fas fa-arrow-circle-left fa-3x" ></i></span>
+    </nav>
+    
+    <header>(Animal) Question!</header>
+
+    <nav id ="goBackButton">
+        <!-- insert a go back button here -->
+    </nav>
+
+    <!-- Question section -->
+    <div id="animalIconQuestion"></div>
+    <div id="question"></div>
+
+    <!-- Answer section -->
+    <div id="answer"></div>
+    <div id="animalIconAnswer"></div>
+    
+
+    <footer></footer>
+</body>
+
+
+</html>
