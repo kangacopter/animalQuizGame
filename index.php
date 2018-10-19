0a340fceae111b17ce3a196bec76212a601c5dc7
Kelly Russell <russellk10@nku.edu>
2018-10-19 01:00:13 -0400

    A few other misc changes.

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width">
-<link rel="stylesheet" href="css/style.css">    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">    
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 <script type="text/javascript" src="js/main.js"></script>
 <head>
     <title>Second Chance's Animal Quiz!</title>
@@ -10,7 +11,7 @@
 
 <body>
 
-    <header>Choose the animal based on dice roll!</header>
+    <header>Roll the dice!</header>
 
     <!-- Animal icon menu for 6 animal question choices -->
     <div id="chooseAnimal">
@@ -19,7 +20,6 @@
         <div class="animalIcon" id="bat">&nbsp;bat</div>
         <div class="animalIcon" id="skunk">&nbsp;skunk</div>
         <div class="animalIcon" id="beaver">&nbsp;beaver</div>
-        <!--<div class="animalIcon" id="fox">&nbsp;fox</div> We may use this instead of beaver-->
         <div class="animalIcon" id="groundhog">&nbsp;groundhog</div>
 
     </div>
