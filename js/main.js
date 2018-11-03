d0cc0f022224f40b8056b37298aed15448c15936
Kelly Russell <russellk10@nku.edu>
2018-11-03 00:30:29 -0400

    Various experiments

---
+++ js/main.js
@@ -0,0 +1,64 @@
+// Function to build the question display (question, buttons for true false)
+function displayQuestion() {
+    // Show Q icon
+
+    // Show question
+
+}
+
+// Displays answer box, intially hidden
+function displayAnswer() {
+    // Show A icon 
+
+    // Show answer
+
+}
+
+// Collection of animal question arrays
+var qRaccoon = [
+    [ "Question 1 for raccoon", true, false, "Answer explaination" ],
+    [ "Question 2 for raccoon", true, false, "Answer explaination" ],
+    [ "Question 3 for raccoon", true, false, "Answer explaination" ],
+    [ "Question 4 for raccoon", true, false, "Answer explaination" ],
+    [ "Question 5 for raccoon", true, false, "Answer explaination" ]
+]
+
+var qOpossum = [
+    [ "Question 1 for opossum", true, false, "Answer explaination" ],
+    [ "Question 2 for opossum", true, false, "Answer explaination" ],
+    [ "Question 3 for opossum", true, false, "Answer explaination" ],
+    [ "Question 4 for opossum", true, false, "Answer explaination" ],
+    [ "Question 5 for opossum", true, false, "Answer explaination" ]
+]
+
+var qBat = [
+    [ "Question 1 for bat", true, false, "Answer explaination" ],
+    [ "Question 2 for bat", true, false, "Answer explaination" ],
+    [ "Question 3 for bat", true, false, "Answer explaination" ],
+    [ "Question 4 for bat", true, false, "Answer explaination" ],
+    [ "Question 5 for bat", true, false, "Answer explaination" ]
+]
+
+var qSkunk = [
+    [ "Question 1 for skunk", true, false, "Answer explaination" ],
+    [ "Question 2 for skunk", true, false, "Answer explaination" ],
+    [ "Question 3 for skunk", true, false, "Answer explaination" ],
+    [ "Question 4 for skunk", true, false, "Answer explaination" ],
+    [ "Question 5 for skunk", true, false, "Answer explaination" ]
+]
+
+var qBeaver = [
+    [ "Question 1 for beaver", true, false, "Answer explaination" ],
+    [ "Question 2 for beaver", true, false, "Answer explaination" ],
+    [ "Question 3 for beaver", true, false, "Answer explaination" ],
+    [ "Question 4 for beaver", true, false, "Answer explaination" ],
+    [ "Question 5 for beaver", true, false, "Answer explaination" ]
+]
+
+var qGroundhog = [
+    [ "Question 1 for groundhog", true, false, "Answer explaination" ],
+    [ "Question 2 for groundhog", true, false, "Answer explaination" ],
+    [ "Question 3 for groundhog", true, false, "Answer explaination" ],
+    [ "Question 4 for groundhog", true, false, "Answer explaination" ],
+    [ "Question 5 for groundhog", true, false, "Answer explaination" ]
+]
